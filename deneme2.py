3809d48c45235c5d8ccb22f88b2d0027c8eb575a
ogukeps <93093968+ogukeps@users.noreply.github.com>
2022-07-20 16:30:36 +0300

    Update deneme2.py

--- deneme2.py
+++ deneme2.py
@@ -1,15 +1,26 @@
 import can
 
+ıd= input("Enter ID: ")
+ıd=int(ıd)
+
+hex_arr = [0]*4
+for i in range(0, 4):
+    hexValue1 = input("Input first hex value: ")
+    hexValue1 = int(hexValue1,16)
+    hex_arr[i] = hexValue1
+    print(hex_arr)
 
 
-bus = can.Bus(interface='socketcan',
-              channel='vcan0',
-              receive_own_messages=True)
+
+bus = can.Bus(interface='socketcan',channel='vcan0', receive_own_messages=True)
 
 
-message = can.Message(arbitration_id=123, is_extended_id=True,
-                      data=[0x11, 0x22, 0x33])
+message = can.Message(arbitration_id=ıd, is_extended_id=True,data=hex_arr)
+
+
+
 bus.send(message, timeout=0.2)
+
 
 
 for msg in bus:
@@ -17,3 +28,8 @@ for msg in bus:
 
 
 notifier = can.Notifier(bus, [can.Logger("recorded.log"), can.Printer()])
+
+bus = can.Bus(interface='socketcan',channel='vcan0')
+read_msg = bus.recv()
+binValue1 = bin(read_msg)
+print("bin ",binValue1[2:8])
